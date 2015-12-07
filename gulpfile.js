958f89c54ceeb5714f5fc121b1753c3a88c697cf
Jeremy Foster <jeremy.foster@live.com>
2015-12-07 15:35:55 -0800

    remove some straggler files

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,7 @@ var paths = {
     styles: ['src/**/*.css']
 };
 
-gulp.task('serve',['clean','tsc','scripts','styles','templates'], function () {
-    gulp.watch(paths.ts, ['tsc']);
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles, ['styles']);
-    gulp.watch(paths.templates, ['templates']);
-});
-
-gulp.task('clean', function() {
-    return gulp.src('build',{read:false})
-        .pipe(clean());
-});
+gulp.task('build', ['tsc','scripts','styles','templates']);
 
 gulp.task('tsc', function () {
     return gulp.src(paths.ts)
@@ -32,17 +22,22 @@ gulp.task('tsc', function () {
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['clean'], function () {
     return gulp.src('src/**/*.js')
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', ['clean'], function () {
     return gulp.src(paths.styles)
         .pipe(gulp.dest('build'));
 })
 
-gulp.task('templates', function () {
+gulp.task('templates', ['clean'], function () {
     return gulp.src(paths.templates)
         .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('build', {read:false})
+        .pipe(clean());
 });
