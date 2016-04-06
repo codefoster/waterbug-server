2eafe7bfc9d42a782a434c063292d159f62d3525
Jeremy Foster <jeremy.foster@live.com>
2016-04-06 10:02:35 -0700

    pass through all socket messages

--- server.js
+++ server.js
@@ -1,39 +1,24 @@
 var express = require("express");
-var path = require("path");
+
 var app = express();
-app.state = {
-    raceDistance: 500,
-    startTime: null,
-    rowers: [
-        {
-            name: 'jeremy',
-            strokeRates: [24.5, 24.0, 25.5, 26.0, 25.5],
-            caloriesPerMinute: 78,
-            distance: 99
-        },
-        {
-            name: 'daniel',
-            strokeRates: [24.5, 24.0, 25.5, 26.0, 25.5],
-            caloriesPerMinute: 78,
-            distance: 300
-        }
-    ]
-}
 
 var server = require("http").Server(app);
 var io = require("socket.io").listen(server);
- 
-// app.use(express.static(path.join(__dirname, "../app")));
-// app.use("/scripts", express.static(path.join(__dirname,"../node_modules/")));
 
-io.on("connection", function (socket) {
-    console.log('client connected');
-    socket.on("msg", function (msg) {
-        console.log('msg from client: ' + msg);
-    });
+io.on("connection", function(socket) {
+    // wire up a handler for the client sending a 'stroke' message
+    console.log('connection');
+    console.log(socket.conn.id);
+
+    // these are simple pass thru's
+    // when any client sends a "stroke" or "startrace" message it will be passed on to all other connected clients
+    // inside these handlers is where we would "intercept" all messages to persist data to a database
+    socket.on("stroke", data => io.emit("stroke", data));
+    socket.on("startrace", data => io.emit("startrace", data));
+    socket.on("stoprace", data => io.emit("stoprace", data));
 });
 
-server.listen(8080, function () {
+server.listen(8080, function() {
     console.log("Listening on port %s...", server.address().port);
 });
 
