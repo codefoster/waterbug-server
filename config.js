8f41cdf1e6e0f246e932e9dc9f3122b94818000c
Daniel Egan <daniel.egan@microsoft.com>
2016-05-11 13:28:47 -0700

    added MongoDB support with MongoLabs for now

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoConnectionString:"mongodb://waterbug-data:RoACnxNa0b6reN8DekWIs6xYrUCieYQn5DBbAlAo7WsnlmZJk1Dz1Yly5EVoK6dyHHXy65XBhy6Nts7kUaHo1Q==@waterbug-data.documents.azure.com:10250/?ssl=true",
+    mongoLabsConnectionString: "mongodb://waterbug-data:WaterBug01@ds030829.mlab.com:30829/waterbug-data",
     devPort:8080
 }
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var server = require("http").Server(app);
 var io = require("socket.io").listen(server);
 var config = require('./config');
 var port = process.env.port || config.devPort;
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = config.mongoLabsConnectionString;
 
 io.on("connection", function(socket) {
     console.log(`connection (id:${socket.conn.id})`);
@@ -13,7 +17,35 @@ io.on("connection", function(socket) {
         //TODO:store in mongo
         // connection string is config.mongoConnectionString
         // look at the data.message to see what type of message it is and thus what collection it should go in
-        
+        console.log(data);
+        var insertDocument = function (db, callback) {
+            if (data.message === 'startrace') {
+                db.collection('startRace').insertOne({
+                    data
+                }, function (err, result) {
+                    assert.equal(err, null);
+                    console.log("Inserted a document into the startRace collection.");
+                    callback();
+                });
+            } else {
+                db.collection('raceData').insertOne({
+                    data
+                }, function (err, result) {
+                    assert.equal(err, null);
+                    console.log("Inserted a document into the raceData collection.");
+                    callback();
+                });
+            }
+
+        };
+        MongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            insertDocument(db, function () {
+                db.close();
+            });
+        });
+
+
         //send the data to all connected clients
         io.send(data);
     });
