96f1cf3914419d7809387896ccf4491a83c3853a
Jeremy Foster <jeremy.foster@live.com>
2016-05-10 17:50:23 -0700

    add mongo connection string to config file, add node typings

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoConnectionString:"mongodb://waterbug-data:RoACnxNa0b6reN8DekWIs6xYrUCieYQn5DBbAlAo7WsnlmZJk1Dz1Yly5EVoK6dyHHXy65XBhy6Nts7kUaHo1Q==@waterbug-data.documents.azure.com:10250/?ssl=true",
+    devPort:8080
+}
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-/// <reference path="typings/main/ambient/express/index.d.ts" />
-
 var express = require("express");
-
 var app = express();
-
 var server = require("http").Server(app);
 var io = require("socket.io").listen(server);
-var port = process.env.port || 8080;
+var config = require('./config');
+var port = process.env.port || config.devPort;
 
 io.on("connection", function(socket) {
     console.log(`connection (id:${socket.conn.id})`);
 
     // pass all messages along
-    socket.on("message", data => io.send(data));
+    socket.on("message", data => {
+        //TODO:store in mongo
+        // connection string is config.mongoConnectionString
+        // look at the data.message to see what type of message it is and thus what collection it should go in
+        
+        //send the data to all connected clients
+        io.send(data);
+    });
 });
 
 app.get('/', (req,res) => {
