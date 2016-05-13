1e94658e4ae7b0e3cc8d296b2d4ee08ee58f3160
Daniel Egan <daniel.egan@microsoft.com>
2016-05-13 15:54:08 -0700

    Added DocumentDB Support and Server live info via socket.io

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
+//Set up server
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/" ,function (req, res) {
+    res.redirect("index.html");
+})
+
+//Include socket.io
 var io = require("socket.io").listen(server);
+
+
+//define our port and mongo information 
 var config = require('./config');
 var port = process.env.port || config.devPort;
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = config.mongoLabsConnectionString;
 
-io.on("connection", function(socket) {
-    console.log(`connection (id:${socket.conn.id})`);
+var routes = require('./routes/index');
 
+io.on("connection", function (socket) {
+    console.log("connection id:" + socket.conn.id);
     // pass all messages along
     socket.on("message", data => {
-        //TODO:store in mongo
-        // connection string is config.mongoConnectionString
-        // look at the data.message to see what type of message it is and thus what collection it should go in
-        console.log(data);
+
         var insertDocument = function (db, callback) {
             if (data.message === 'startrace') {
+
                 db.collection('startRace').insertOne({
                     data
                 }, function (err, result) {
-                    assert.equal(err, null);
                     console.log("Inserted a document into the startRace collection.");
                     callback();
                 });
@@ -31,7 +39,6 @@ io.on("connection", function(socket) {
                 db.collection('raceData').insertOne({
                     data
                 }, function (err, result) {
-                    assert.equal(err, null);
                     console.log("Inserted a document into the raceData collection.");
                     callback();
                 });
@@ -39,25 +46,17 @@ io.on("connection", function(socket) {
 
         };
         MongoClient.connect(url, function (err, db) {
-            assert.equal(null, err);
             insertDocument(db, function () {
                 db.close();
             });
         });
-
-
         //send the data to all connected clients
         io.send(data);
     });
 });
 
-app.get('/', (req,res) => {
-    res.header('200','{"Content-Type","text/html"}');
-    res.send("Socket server active and listening...");
-    res.end();
-});
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log("Listening on port %s...", server.address().port);
 });
 
